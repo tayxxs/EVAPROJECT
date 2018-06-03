1dfc37e2788e792505689e90183e177f322efbb6
tayxxs <tayxxs@outlook.com>
2018-06-04 00:06:33 +0800

    jpa init

---
+++ server/src/main/java/com/eva/entities/User.java
@@ -0,0 +1,44 @@
+package com.eva.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String userName;
+	private String nickName;
+	
+	protected User() {}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	
+}
